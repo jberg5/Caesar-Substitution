2352415a590c8d34cec3c33c006d235308210562
Jonathan Berg <Jonathan Berg>
2017-01-30 20:14:35 -0500

    It works!

--- substitution.py
+++ substitution.py
@@ -1,11 +1,13 @@
 class Cypher:
 
     def __init__(self, message, key):
+        """Initializes the Cypher, storing the original message and key"""
         self.message = message.replace(' ','').lower()
         self.key = key
         self.LetNum = {k:v for k,v in zip([char for char in "abcdefghijklmnopqrstuvwxyz"],range(26))}
         self.NumLet = {k:v for k,v in zip(range(26),[char for char in "abcdefghijklmnopqrstuvwxyz"])}
         self.encryption = None
+        self.decryption = None
 
     def toNum(self,s):
         return [self.LetNum[char] for char in s]
@@ -16,12 +18,16 @@ class Cypher:
         return final.upper()
 class Encrypt(Cypher):
 
-    def __init__(self):
+    def encrypt(self):
         self.encryption = self.toString([(i + self.key) % 26 for i in self.toNum(self.message)])
-        
     def __repr__(self):
+        return ' '.join([self.encryption[i:i+5] for i in range(0, len(self.encryption), 5)])
+
 
 class Decrypt(Cypher):
 
-    def __init__(self):
+    def decrypt(self):
         self.decryption = self.toString([(i - self.key) % 26 for i in self.toNum(self.message)])
+
+    def __repr__(self):
+        return self.decryption
